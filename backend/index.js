3cc2cfaf7799f3aedd62304d8f0a529789291c7e
Zainab Bazoun <hw6362@wayne.edu>
2026-02-13 22:36:42 -0500

    Updating Files

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("Connected to MongoDB"))
   .catch(err => console.error(err));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () =>{
 
