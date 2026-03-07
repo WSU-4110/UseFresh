1b98c686b989fac16cbe3fb15a31591ef84dd58a
Zainab Bazoun <hw6362@wayne.edu>
2026-03-07 17:49:22 -0500

    Methods to get, delete, and update food items were implemented.

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -17,4 +17,35 @@ exports.addFood = async (req, res) => {
   }
 };
 
+exports.getFoods = async (req, res) => {
+  try {
+    const foods = await FoodItem.find();
+    res.json(foods);
+  } catch (error) {
+    res.status(500).json({ error: "Unable to fetch food items" });
+  }
+};
 
+exports.deleteFood = async (req, res) => {
+  try {
+    await FoodItem.findByIdAndDelete(req.params.id);
+    res.json({ message: "Food item deleted" });
+  } catch (error) {
+    res.status(500).json({ error: "Unable to delete food item" });
+  }
+};
+
+exports.updateFood = async (req, res) => {
+  try {
+    const updatedFood = await FoodItem.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+
+    res.json(updatedFood);
+
+  } catch (error) {
+    res.status(500).json({ error: "Unable to update food item" });
+  }
+};
--- backend/routes/foodRoutes.js
+++ backend/routes/foodRoutes.js
@@ -3,14 +3,14 @@ const router = express.Router();
 
 const {
     addFood,
-    // getFoods,
-    // deleteFood,
-    // updateFood
+    getFoods,
+    deleteFood,
+    updateFood
 } = require("../controllers/foodController");
 
 router.post("/add", addFood);
-// router.get("/all", getFoods);
-// router.delete("/:id", deleteFood);
-// router.put("/:id", updateFood);
+router.get("/all", getFoods);
+router.delete("/:id", deleteFood);
+router.put("/:id", updateFood);
 
 module.exports = router;
