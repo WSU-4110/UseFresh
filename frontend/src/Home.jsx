a1e5cffec113bfe20a67e664dad8d9429b92dfb2
Hamsini Gupta <ho7692@wayne.edu>
2026-03-07 16:12:59 -0500

    added new version

---
+++ frontend/src/Home.jsx
@@ -0,0 +1,12 @@
+//importing style
+import "./Home.css";
+
+export default function Home() {
+  //returns what will show on the Home page
+  return (
+    <main className="home-page">
+      <h1>Home</h1>
+      <p>Welcome to UseFresh.</p>
+    </main>
+  );
+}
---
+++ frontend/src/ViewItems.jsx
@@ -0,0 +1,12 @@
+//importing style
+import "./ViewItems.css";
+
+export default function ViewItems() {
+    //returns what is going to be shown on the view items page
+  return (
+    <main className="view-items-page">
+      <h1>View Items</h1>
+      <p>Items availabe in your feed.</p>
+    </main>
+  );
+}
