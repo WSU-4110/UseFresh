7b9aa7f2edfeed100435f3a7865bcc07e7cee6f5
Sanika Annadate <sannadate2023@gmail.com>
2026-03-08 21:15:18 -0400

    Connecting AI to the database

--- AI_component/server.js
+++ AI_component/server.js
@@ -2,9 +2,23 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { HfInference } from "@huggingface/inference";
+import mongoose from "mongoose";
 
 dotenv.config();
 console.log("HF_TOKEN exists?", !!process.env.HF_TOKEN);
+
+//connecting to the database code
+mongoose.connect(process.env.MONGO_URI, {dbName: "usefresh"} )
+  .then(() => console.log("Connected to MongoDB"))
+  .catch(err => console.error(err));
+
+const foodSchema = new mongoose.Schema({
+  foodItem: String,
+  quantity: Number,
+  expirationDate: Date
+});
+
+const Food = mongoose.model("Food", foodSchema,"fooditems");
 
 const app = express();
 app.use(cors());
@@ -15,16 +29,20 @@ const hf = new HfInference(process.env.HF_TOKEN);
 app.post("/suggest-recipes", async (req, res) => {
   console.log("HIT /suggest-recipes");
   try {
-    // For now: static prompt, no getting data from database yet, will change for sprint2
-    const prompt = `
-Create ONE simple recipe.
-Return ONLY valid JSON in this format:
-{
-  "title": string,
-  "ingredients": string[],
-  "steps": string[]
-}
-`.trim();
+    const foods = await Food.find();
+    console.log("Foods from DB:", foods);
+    const ingredients = foods.map(f => f.foodItem).join(", ");
+
+  const prompt = `
+  Create ONE simple recipe using these ingredients if possible: ${ingredients}.
+
+  Return ONLY valid JSON in this format:
+  {
+    "title": string,
+    "ingredients": string[],
+    "steps": string[]
+  }
+  `.trim();
 
     const chat = await hf.chatCompletion({
       model: "Qwen/Qwen2.5-7B-Instruct:together",
